b3fd8af8ef8b76a3200eb354d1ba5a988ac97b29
Alex Cucer <alexander.cucer@titanium-soft.com>
2020-02-12 16:38:42 +0200

    Initial implementation

--- MyPackBox@alexanderc/extension.js
+++ MyPackBox@alexanderc/extension.js
@@ -1,53 +1,152 @@
+'use strict';
 
-const St = imports.gi.St;
+const ExtensionUtils = imports.misc.extensionUtils;
+const GObject = imports.gi.GObject;
+const Cairo = imports.cairo;
+const Mainloop = imports.mainloop;
 const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
+const Lang = imports.lang;
+const St = imports.gi.St;
+const PopupMenu = imports.ui.popupMenu;
+const PanelMenu = imports.ui.panelMenu;
+const Util = imports.misc.util;
+const Json = imports.gi.Json;
+const Soup = imports.gi.Soup;
+const Gio = imports.gi.Gio;
+//const Gettext = imports.gettext.domain($.metadata.name);
+const Config = imports.misc.config;
 
-let text, button;
+//const _ = Gettext.gettext;
+const _ = x => x; // mock here
+const $ = ExtensionUtils.getCurrentExtension();
 
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
-}
+const SHELL_MINOR = parseInt(Config.PACKAGE_VERSION.split('.')[1]);
+const SETTING_REFRESH_INTERVAL = 'refresh-interval';
+const SETTING_USERNAME = 'username';
+const SETTING_PASSWORD = 'password';
+const DEFAULT_POOL_INTERVAL = 180;
 
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
+var MyPackBox = class MyPackBox extends PanelMenu.Button {
+  _init() {
+    super._init(0.0, $.metadata.name, false);
+
+    // Initialize button
+    PanelMenu.Button.prototype._init.call(this, 0.0);
+
+    // Soup session (see https://bugzilla.gnome.org/show_bug.cgi?id=661323#c64)
+    this._httpSession = new Soup.SessionAsync();
+    Soup.Session.prototype.add_feature.call(this._httpSession, new Soup.ProxyResolverDefault());
+
+    // Setup button
+    this._label = new St.Label({ style_class: 'panel-label', text: _($.metadata.name) });
+    const topBox = new St.BoxLayout();
+    topBox.add_actor(this._label);
+    this.actor.add_actor(topBox);
+    Main.panel._centerBox.add(this.actor, { y_fill: true });
+    Main.panel._menus.addMenu(this.menu);
+
+    // Setup widget
+    this._widget = new St.Bin({ style_class: 'widget' });
+    const mainBox = new St.BoxLayout({ vertical: true });
+    mainBox.add_actor(this._widget);
+    this.menu.addActor(mainBox);
+
+    // Load settings
+    this._settings = ExtensionUtils.getSettings();
+    const settingsWatcher = Lang.bind(this, function () {
+      this._refresh_interval = this._settings.get_int(SETTING_REFRESH_INTERVAL) || DEFAULT_POOL_INTERVAL;
+      this._username = this._settings.get_string(SETTING_USERNAME);
+      this._password = this._settings.get_string(SETTING_PASSWORD);
+      this.refreshUi();
+    });
+    this._settings.connect('changed::' + SETTING_REFRESH_INTERVAL, settingsWatcher);
+    this._settings.connect('changed::' + SETTING_USERNAME, settingsWatcher);
+    this._settings.connect('changed::' + SETTING_PASSWORD, settingsWatcher);
+    settingsWatcher();
+
+    // Show progress
+    this.showLoadingUi();
+
+    // Run ticker
+    this.refreshUi(true);
+  }
+
+  async refreshUi(recurse) {
+    // If no username or password it doesn't make sense
+    // to go further. Also stop the 
+    if (!this._username || !this._password) {
+      this.destroyWidget();
+      this._widget.set_child(new St.Label({ text: _('You need to set credentials first') }));
+    } else {
+      // @todo Load and data
+      this.rebuildCurrentUi({});
     }
 
-    text.opacity = 255;
+    if (recurse) {
+      Mainloop.timeout_add_seconds(this._refresh_interval, Lang.bind(this, function () {
+        this.refresh(recurse);
+      }));
+    }
+  }
 
-    let monitor = Main.layoutManager.primaryMonitor;
+  destroyWidget() {
+    if (this._widget.get_child() != null) {
+      this._widget.get_child().destroy();
+    }
+  }
 
-    text.set_position(monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
-                      monitor.y + Math.floor(monitor.height / 2 - text.height / 2));
+  showLoadingUi() {
+    this.destroyWidget();
+    this._widget.set_child(new St.Label({ text: _('Loading...') }));
+  }
 
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
+  rebuildCurrentUi(data) {
+    this.destroyWidget();
+    // @todo Handle data
+  }
+
+  // async loadJsonAsync(url, cb) {
+  //   let here = this;
+  //   let message = Soup.Message.new('GET', url);
+
+  //   this._httpSession.queue_message(message, function (_session, message) {
+  //     let jp = new Json.Parser();
+  //     jp.load_from_data(message.response_body.data, -1);
+  //     cb.call(here, jp.get_root().get_object());
+  //   });
+  // },
+
+  destroy() {
+    this._settings.run_dispose();
+    super.destroy();
+  }
 }
 
-function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon({ icon_name: 'system-run-symbolic',
-                             style_class: 'system-status-icon' });
+if (SHELL_MINOR > 30) {
+  MyPackBox = GObject.registerClass(
+    { GTypeName: 'MyPackBox' },
+    MyPackBox
+  );
+}
 
-    button.set_child(icon);
-    button.connect('button-press-event', _showHello);
+let myPackBoxMenu;
+
+function init() {
+  log(`initializing ${$.metadata.name} version ${$.metadata.version}`);
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+  log(`enabling ${$.metadata.name} version ${$.metadata.version}`);
+
+  myPackBoxMenu = new MyPackBox();
+  Main.panel.addToStatusArea($.metadata.name, myPackBoxMenu);
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+  log(`disabling ${$.metadata.name} version ${$.metadata.version}`);
+
+  if (myPackBoxMenu !== null) {
+    myPackBoxMenu.destroy();
+    myPackBoxMenu = null;
+  }
 }
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# pt.md-gnome-extension-
+[![Donate with Ethereum](https://en.cryptobadges.io/badge/micro/0x4A1eADE6B3780b50582344c162a547D04e4E8E4a)](https://en.cryptobadges.io/donate/0x4A1eADE6B3780b50582344c162a547D04e4E8E4a)
+
+# pt.md-gnome-extension
 Gnome Shell Extension: Manage your packages handled by Moldovan PostTerminal (pt.md)
+
+### Prerequisites
+
+- [ ] Gnome v3.x
+- [ ] *zip + unzip (development only)*
+
+### Installation
+
+To install from command line:
+
+```bash
+git clone git@github.com:AlexanderC/pt.md-gnome-extension.git
+unzip -q pt.md-gnome-extension/dist/MyPackBox@alexanderc.zip -d ~/.local/share/gnome-shell/extensions/MyPackBox@alexanderc/
+gnome-shell-extension-tool -e MyPackBox@alexanderc
+```
+
+Or install it from [https://extensions.gnome.org/](https://extensions.gnome.org/).
+
+### Roadmap
+
+- [ ] Add multilanguage support
+
+### Support
+
+I really love open source, however i do need your help to keep the library up to date. There are several ways to do it: open issues, submit PRs, share the library w/ community or simply [![Donate with Ethereum](https://en.cryptobadges.io/badge/micro/0x4A1eADE6B3780b50582344c162a547D04e4E8E4a)](https://en.cryptobadges.io/donate/0x4A1eADE6B3780b50582344c162a547D04e4E8E4a)
